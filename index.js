d5d0493c2f8729024311ade4c40894ec6b2e82b5
haricharanbole <43782473+haricharanbole@users.noreply.github.com>
2019-09-18 03:36:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const NodeWebcam = require("node-webcam");
+var Jimp = require("jimp");
 
 var opts = {
   width: 640,
   height: 320,
   quality: 300,
-  output: "jpeg",
+  output: "bmp",
   device: false,
   callbackReturn: "base64",
   verbose: false,
@@ -17,6 +18,8 @@ var opts = {
 };
 
 const FPS = 1;
+var count = "init";
+
 var Webcam = NodeWebcam.create(opts);
 
 app.get("/", (req, res) => {
@@ -28,6 +31,18 @@ setInterval(() => {
     "image",
     (err, imgData) => {
       io.emit("image", imgData);
+      console.log(count);
+      if (count === 10 || count === "init") {
+        Jimp.read("image.bmp", (err, lenna) => {
+          if (err) throw err;
+          lenna
+            .resize(640, 320) // resize
+            .quality(100) // set JPEG quality
+            .write("out.jpg"); // save
+        });
+        count = 0;
+      }
+      count = count + 1;
       console.log("Event Emitted");
     },
     opts
